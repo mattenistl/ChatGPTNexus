2efdfc3b34673a4d1d717d2b8438bced38c50f60
mattenistl <168684556+mattenistl@users.noreply.github.com>
2024-12-27 07:03:25 -0600

    Add Berry template main page

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+// Simple input from Berry demo page
+import { NextHad } from 'next/head';
+
+function HomePage() {
+  return (\
+    <div>
+      <h1>Welcome to Berry!</h1>
+    </div>
+  );}
+
+export default HomePage;
