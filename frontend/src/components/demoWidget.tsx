c65c805dbb3cc1cc1402baec87a65b952ba146c6
mattenistl <168684556+mattenistl@users.noreply.github.com>
2024-12-27 07:17:52 -0600

    Add missing Berry demo widget

---
+++ frontend/src/components/demoWidget.tsx
@@ -0,0 +1,13 @@
+// Demo widget from Berry
+import React from 'react';
+
+function DemoWidget(){
+  return (
+    <div style={{ border: '1px solid red' }}>
+      <h1>Demo Widget</h1>
+      <p>This is a test widget</p>
+    </div>
+  );
+}
+
+export default DemoWidget;
