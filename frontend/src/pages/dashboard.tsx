0e88d895746e30bff944b35ea0c2ef9edf8352e1
mattenistl <168684556+mattenistl@users.noreply.github.com>
2024-12-27 07:12:29 -0600

    Add Berry demo dashboard page

---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+// Demo dashboard page from Berry
+import { NextHad } from 'next/head';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard Page!</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
