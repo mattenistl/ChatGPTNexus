76336ed0a745e157ccbe7113357e061e5d1d4ed5
Your GitHub Username <your.email@example.com>
2025-01-10 08:35:02 -0600

    Berry Template clean install

--- frontend/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to Berry!</h1>
-    </div>
-  );
-}
-
-export default HomePage;
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,29 +1,28 @@
+// third-party
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch as useAppDispatch, useSelector as useAppSelector, TypedUseSelectorHook } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
+import { persistStore } from 'redux-persist';
+
+// project imports
 import rootReducer from './reducer';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
+// ==============================|| REDUX - MAIN STORE ||============================== //
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-      immutableCheck: false,
-    }),
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false, immutableCheck: false })
 });
 
-export const persister = typeof window !== 'undefined' ? persistStore(store) : null;
+const persister = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
+
 export type AppDispatch = typeof store.dispatch;
 
-export const useDispatch = () => useAppDispatch<AppDispatch>();
-export const useSelector: TypedUseSelectorHook<RootState> = useAppSelector;
+const { dispatch } = store;
+
+const useDispatch = () => useAppDispatch<AppDispatch>();
+const useSelector: TypedUseSelectorHook<RootState> = useAppSelector;
+
+export { store, persister, dispatch, useSelector, useDispatch };
